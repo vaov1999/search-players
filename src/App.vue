3dab8f261269aa48677ea1750d68464f855b9ef7
vaov1999 <vaov1999@gmail.com>
2021-07-03 14:25:05 +0300

    input request timeout

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,10 @@
 <template>
-  <div id="app">
-    <input
-      type="text"
-      class="input"
-      placeholder="Please type a player name"
-      v-model="searchText"
-      @input="searchPlayer"
-    />
-    <ul class="users-list">
-      <li v-for="(player, index) in players" :key="index">{{ player.name }}</li>
-    </ul>
-  </div>
+  <router-view />
 </template>
 
 <script>
 export default {
   name: "App",
-  data() {
-    return {
-      searchText: "",
-      players: [],
-    };
-  },
-  methods: {
-    searchPlayer() {
-      if (this.searchText.length > 2) {
-        fetch(
-          `https://vortex.worldofwarships.ru/api/accounts/search/${this.searchText}/`
-        )
-          .then((res) => res.json())
-          .then((res) => {
-            this.players = res.data.slice(0, 10);
-          });
-      } else {
-        this.players = [];
-      }
-    },
-  },
 };
 </script>
 
@@ -48,57 +16,11 @@ body {
   padding: 0;
   margin: 0;
   height: 100vh;
-}
-
-#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #333;
-  padding: 20px;
   min-height: 100%;
-  background: rgb(81, 249, 255);
-  background: linear-gradient(
-    24deg,
-    rgba(81, 249, 255, 1) 0%,
-    rgba(248, 0, 255, 0.7) 100%
-  );
-}
-
-.input {
-  width: 100%;
-  max-width: 400px;
-  padding: 25px;
-  border: none;
-  border-radius: 15px;
-  box-shadow: 5px 5px 20px rgba(#2c3e50, 0.4);
-  outline: none;
-  font-size: 25px;
-  &:focus {
-    box-shadow: 5px 5px 20px rgba(#2c3e50, 1);
-  }
-  &::placeholder {
-    text-align: center;
-    color: #999;
-  }
-}
-
-.users-list {
-  width: 100%;
-  max-width: 400px;
-  margin: 30px auto;
-  padding: 0;
-  li {
-    display: block;
-    padding: 10px;
-    margin-top: 10px;
-    border-radius: 15px;
-    background-color: #fff;
-    &:hover {
-      cursor: pointer;
-      box-shadow: 5px 5px 20px rgba(#2c3e50, 0.4);
-    }
-  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input
+      type="text"
+      class="input"
+      placeholder="Please type a player name"
+      v-model="searchText"
+      @input="request"
+    />
+    <div class="users-list">
+      <router-link
+        class="link"
+        v-for="(player, index) in players"
+        :key="index"
+        :to="`/players/${player.name.toLowerCase()}`"
+        >{{ player.name }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      searchText: "",
+      players: [],
+      timeout: null,
+    };
+  },
+  methods: {
+    request() {
+      const vm = this;
+
+      clearTimeout(this.timeout);
+
+      this.timeout = setTimeout(function () {
+        vm.searchPlayer();
+      }, 1000);
+    },
+    searchPlayer() {
+      if (this.searchText.length > 2) {
+        fetch(
+          `https://vortex.worldofwarships.ru/api/accounts/search/${this.searchText}/`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            this.players = res.data.slice(0, 10);
+          });
+      } else {
+        this.players = [];
+      }
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  padding: 20px;
+  background: rgb(81, 249, 255);
+  background: linear-gradient(
+    24deg,
+    rgba(81, 249, 255, 1) 0%,
+    rgba(248, 0, 255, 0.7) 100%
+  );
+
+  .input {
+    width: 100%;
+    max-width: 400px;
+    padding: 25px;
+    border: none;
+    border-radius: 15px;
+    box-shadow: 5px 5px 20px rgba(#2c3e50, 0.4);
+    outline: none;
+    font-size: 25px;
+    &:focus {
+      box-shadow: 5px 5px 20px rgba(#2c3e50, 1);
+    }
+    &::placeholder {
+      text-align: center;
+      color: #999;
+    }
+  }
+
+  .users-list {
+    width: 100%;
+    max-width: 400px;
+    margin: 30px auto;
+    padding: 0;
+    .link {
+      display: block;
+      padding: 10px;
+      margin-top: 10px;
+      border-radius: 15px;
+      background-color: #fff;
+      color: #333333;
+      text-decoration: none;
+      &:hover {
+        cursor: pointer;
+        box-shadow: 5px 5px 20px rgba(#2c3e50, 0.4);
+      }
+    }
+  }
+}
+</style>
